535d32d3f5226f5d080f7f19fbc99cc3b0405285
Nicolas <76515190+NicolasMB3@users.noreply.github.com>
2022-12-09 14:37:22 +0100

    Fetch json data :+1:

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,28 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
 
+    const { name, portrait, id, city, country, tagline, price } = data;
     const picture = `assets/photographers/${portrait}`;
+    const photographerLink = "photographer.html"
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        const h2 = document.createElement('h2');
+        const p = document.createElement('p');
+        const p2 = document.createElement('p');
+        const p3 = document.createElement('p');
+        const a = document.createElement('a');
+        img.setAttribute("src", picture);
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        p.textContent = `${city}, ${country}`;
+        p2.textContent = tagline;
+        p3.textContent = price + '€/jour'
+        a.classList.add('linkArticle')
+        a.href = `${photographerLink}?id=${id}`;
+        a.append(img, h2);
+        article.append(a, p, p2, p3);
         return (article);
     }
+
     return { name, picture, getUserCardDOM }
 }
